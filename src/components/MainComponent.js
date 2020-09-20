93e4a26148787986692140c69cf30dd5c2ac966e
Enno Huang <hi.xiaolonghuang@gmail.com>
2020-09-20 23:15:30 +1200

    Finished the 2nd assignment

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Menu from "./MenuComponent";
 import Contact from "./ContactComponent";
 import Dishdetail from "./DishdetailComponent";
+import About from "./AboutComponent";
 import {DISHES} from "../shared/dishes";
 import {COMMENTS} from "../shared/comments";
 import {PROMOTIONS} from "../shared/promotions";
@@ -49,6 +50,7 @@ class Main extends Component {
                 <Header/>
                 <Switch>
                     <Route path={"/home"} component={HomePage} />
+                    <Route path={"/aboutus"} component={()=> <About leaders={this.state.leaders}/>}/>
                     <Route exact path={"/menu"} component={() => <Menu dishes={this.state.dishes}/>}/>
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path={"/contactus"} component={Contact}/>
